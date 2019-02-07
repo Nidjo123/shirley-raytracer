8011fcba69ef33ebd2e2eb227bf880c988a72580
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2019-02-08 00:54:36 +0100

    Add ray class

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include "vec3.hpp"
+#include "ray.hpp"
+
+vec3 color(const ray& r) {
+  vec3 dir = normalize(r.direction());
+  float t = 0.5f*(dir.y + 1.0f);
+  return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
+}
 
 int main() {
   int nx = 200;
   int ny = 100;
 
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+  vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+  vec3 horizontal(4.0f, 0.0f, 0.0f);
+  vec3 vertical(0.0f, 2.0f, 0.0f);
+  vec3 origin(0.0f, 0.0f, 0.0f);
+
   for (int j = ny-1; j >= 0; j--)
     for (int i = 0; i < nx; i++) {
-      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
+      ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+      vec3 col = color(r);
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
---
+++ src/ray.hpp
@@ -0,0 +1,35 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include "vec3.hpp"
+
+class ray {
+public:
+  ray() = default;
+  ray(const vec3& a, const vec3& b);
+  vec3 origin() const;
+  vec3 direction() const;
+  vec3 at(float t) const;
+
+  vec3 A;
+  vec3 B;
+};
+
+ray::ray(const vec3& a, const vec3& b)
+  : A(a), B(b) {
+
+}
+
+vec3 ray::origin() const {
+  return A;
+}
+
+vec3 ray::direction() const {
+  return B;
+}
+
+vec3 ray::at(float t) const {
+  return A + t*B;
+}
+
+#endif // RAY_HPP
