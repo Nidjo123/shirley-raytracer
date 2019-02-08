05c4da36a728c9dba31e031911dd79187503ef52
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2019-02-08 19:13:20 +0100

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,38 @@ vec3 color(const ray& r, hitable *world, int depth) {
   return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
 }
 
+hitable *random_scene() {
+  const int n = 500;
+  hitable **list = new hitable*[n+1];
+  list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5f, 0.5f, 0.5f)));
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = drand48();
+      vec3 center(a+0.9f*drand48(), 0.2f, b+0.9f*drand48());
+      if ((center-vec3(4.f, 0.2f, 0.f)).length() > 0.9) {
+	if (choose_mat < 0.8) { // diffuse
+	  list[i++] = new sphere(center, 0.2f, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+	} else if (choose_mat < 0.95) { // metal
+	  list[i++] = new sphere(center, 0.2f, new metal(vec3(0.5f*(1.f+drand48()), 0.5f*(1.f+drand48()), 0.5f*(1.f+drand48())), drand48()));
+	} else { // dielectric
+	  list[i++] = new sphere(center, 0.2f, new dielectric(1.f+3.f*drand48()));
+	}
+      }
+    }
+  }
+
+  list[i++] = new sphere(vec3(0.f, 1.f, 0.f), 1.f, new dielectric(1.5f));
+  list[i++] = new sphere(vec3(-4.f, 1.f, 0.f), 1.f, new lambertian(vec3(0.4f, 0.2f, 0.1f)));
+  list[i++] = new sphere(vec3(4.f, 1.f, 0.f), 1.f, new metal(vec3(0.7f, 0.6f, 0.5f), 0.f));
+
+  return new hitable_list(list, i);
+}
+
 int main() {
-  int nx = 200;
-  int ny = 100;
-  int ns = 100;
+  int nx = 800;
+  int ny = 600;
+  int ns = 32;
 
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
@@ -38,14 +66,14 @@ int main() {
   list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.55f, new dielectric(1.5f));
   list[4] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.2f, 0.5f, 0.9f), 0.05f));
 
-  hitable *world = new hitable_list(list, 5);
+  hitable *world =  random_scene();//new hitable_list(list, 5);
 
-  vec3 lookfrom = vec3(-2, 2, 1);
-  vec3 lookat = vec3(0, 0, -1);
+  vec3 lookfrom = vec3(4, 0.5f, 5);
+  vec3 lookat = vec3(0, 0.65f, 0);
   vec3 vup = vec3(0, 1, 0);
-  float dist_to_focus = (lookfrom-lookat).length()*1.75f;
-  float aperture = 2.f;
-  camera cam(lookfrom, lookat, vup, 60.f, float(nx)/float(ny), aperture, dist_to_focus);
+  float dist_to_focus = (lookfrom-lookat).length()*2.f;
+  float aperture = 0.01f;
+  camera cam(lookfrom, lookat, vup, 45.f, float(nx)/float(ny), aperture, dist_to_focus);
 
   for (int j = ny-1; j >= 0; j--)
     for (int i = 0; i < nx; i++) {
@@ -63,10 +91,6 @@ int main() {
       int ib = int(255.99*col[2]);
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
-
-  // be pedantic
-  delete list[0];
-  delete list[1];
 
   return 0;
 }
