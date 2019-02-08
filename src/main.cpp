205608e22c3f4565bd5d05e13fc785c38bdcd59c
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2019-02-08 17:11:04 +0100

    Add dielectric material

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,9 @@ int main() {
 
   hitable *list[4];
   list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, new lambertian(vec3(0.8f, 0.3f, 0.3f)));
-  list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, new lambertian(vec3(0.8f, 0.8f, 0.0f)));
+  list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, new lambertian(vec3(0.1f, 0.8f, 0.0f)));
   list[2] = new sphere(vec3(1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 0.3f));
-  list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.8f, 0.8f), 0.7f));
+  list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new dielectric(1.5f));
 
   hitable *world = new hitable_list(list, 4);
 
--- src/material.hpp
+++ src/material.hpp
@@ -65,6 +65,66 @@ bool metal::scatter(const ray& r_in, const hit_record &rec, vec3& attenuation, r
   return dot(scattered.direction(), rec.normal) > 0;
 }
 
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+  vec3 uv = normalize(v);
+  float dt = dot(uv, n);
+  float discriminant = 1.f - ni_over_nt*ni_over_nt*(1.f-dt*dt);
+  if (discriminant > 0) {
+    refracted = ni_over_nt*(uv - dt*n) - n*std::sqrt(discriminant);
+    return true;
+  }
+  return false;
+}
+
+float schlick(float cosine, float ref_idx) {
+  float r0 = (1.f-ref_idx) / (1.f+ref_idx);
+  r0 = r0*r0;
+  return r0 + (1.f-r0)*pow((1.f-cosine),5.f);
+}
+
+class dielectric : public material {
+public:
+  dielectric(float ri);
+  virtual bool scatter(const ray& r_in, const hit_record &rec, vec3& attenuation, ray& scattered) const;
+
+  float ref_idx;
+};
+
+dielectric::dielectric(float ri) : ref_idx{ri} {
+
+}
+
+bool dielectric::scatter(const ray& r_in, const hit_record &rec, vec3& attenuation, ray& scattered) const {
+  vec3 outward_normal;
+  vec3 reflected = reflect(r_in.direction(), rec.normal);
+  float ni_over_nt;
+  attenuation = vec3(1.0, 1.0, 1.0);
+  vec3 refracted;
+  float reflect_prob;
+  float cosine;
+  if (dot(r_in.direction(), rec.normal) > 0) {
+    outward_normal = -rec.normal;
+    ni_over_nt = ref_idx;
+    //         cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    cosine = dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    cosine = sqrt(1 - ref_idx*ref_idx*(1-cosine*cosine));
+  }
+  else {
+    outward_normal = rec.normal;
+    ni_over_nt = 1.0 / ref_idx;
+    cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+  }
+  if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
+    reflect_prob = schlick(cosine, ref_idx);
+  else
+    reflect_prob = 1.0;
+  if (drand48() < reflect_prob)
+    scattered = ray(rec.p, reflected);
+  else
+    scattered = ray(rec.p, refracted);
+  return true;
+}
+
 vec3 random_in_unit_sphere() {
   vec3 p;
   do {
