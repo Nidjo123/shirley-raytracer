1a4cef794987fad41df8d148c96ac79042ade781
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2019-02-08 02:41:13 +0100

    Fix diffuse material appearance

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ vec3 random_in_unit_sphere() {
 
 vec3 color(const ray& r, hitable *world) {
   hit_record rec;
-  if (world->hit(r, 0.0f, MAXFLOAT, rec)) {
+  if (world->hit(r, 0.001f, MAXFLOAT, rec)) {
     vec3 target = rec.p + rec.normal + random_in_unit_sphere();
     return 0.5f*color(ray(rec.p, target-rec.p), world);
   }
@@ -52,6 +52,7 @@ int main() {
 	col += color(r, world);
       }
       col /= float(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
