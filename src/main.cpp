e06901be41d07e1a7e8419f3c50139bb7ec904b1
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2019-02-08 17:21:54 +0100

    Make bubble

--- src/main.cpp
+++ src/main.cpp
@@ -25,19 +25,20 @@ vec3 color(const ray& r, hitable *world, int depth) {
 }
 
 int main() {
-  int nx = 200;
-  int ny = 100;
+  int nx = 800;
+  int ny = 400;
   int ns = 100;
 
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  hitable *list[4];
+  hitable *list[5];
   list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, new lambertian(vec3(0.8f, 0.3f, 0.3f)));
   list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, new lambertian(vec3(0.1f, 0.8f, 0.0f)));
   list[2] = new sphere(vec3(1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 0.3f));
   list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new dielectric(1.5f));
+  list[4] = new sphere(vec3(-1.0f, 0.0f, -1.0f), -0.45f, new dielectric(1.7f));
 
-  hitable *world = new hitable_list(list, 4);
+  hitable *world = new hitable_list(list, 5);
 
   camera cam;
 
