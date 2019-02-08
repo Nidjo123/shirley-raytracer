51338bf5a106982fe0025bc29c54416d6820b80c
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2019-02-08 02:17:33 +0100

    Add antialiasing by sampling multiple rays per pixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cstdlib>
 #include <cfloat>
 #include "vec3.hpp"
 #include "ray.hpp"
 #include "sphere.hpp"
 #include "hitable.hpp"
 #include "hitable_list.hpp"
+#include "camera.hpp"
 
 
 vec3 color(const ray& r, hitable *world) {
@@ -20,25 +22,27 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
   int nx = 200;
   int ny = 100;
+  int ns = 100;
 
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-
-  vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-  vec3 horizontal(4.0f, 0.0f, 0.0f);
-  vec3 vertical(0.0f, 2.0f, 0.0f);
-  vec3 origin(0.0f, 0.0f, 0.0f);
 
   hitable *list[2];
   list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f);
   list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f);
   hitable *world = new hitable_list(list, 2);
 
+  camera cam;
+
   for (int j = ny-1; j >= 0; j--)
     for (int i = 0; i < nx; i++) {
-      float u = float(i) / float(nx);
-      float v = float(j) / float(ny);
-      ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-      vec3 col = color(r, world);
+      vec3 col(0, 0, 0);
+      for (int s = 0; s < ns; s++) {
+	float u = float(i + drand48()) / float(nx);
+	float v = float(j + drand48()) / float(ny);
+	ray r = cam.get_ray(u, v);
+	col += color(r, world);
+      }
+      col /= float(ns);
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
